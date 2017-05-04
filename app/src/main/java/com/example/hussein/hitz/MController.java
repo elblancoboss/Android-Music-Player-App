640caa4fd6848e337b2ce3055a3c567728ce8ddd
Hussein Mahdi <hussein_realmadrid@yahoo.com>
2017-05-04 14:52:23 +0100

    application upload

---
+++ app/src/main/java/com/example/hussein/hitz/MController.java
@@ -0,0 +1,18 @@
+package com.example.hussein.hitz;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by Hussein on 24/03/2017.
+ */
+
+public class MController extends MediaController{
+
+    public MController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
